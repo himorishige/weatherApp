6cac9c953be9e86fa73a85835a460d99172c464d
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-05-05 21:22:21 +0900

    update: storybook settings

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import 'destyle.css';
 import { Global } from '@emotion/react';
 import { globalStyle } from 'src/styles';
 
@@ -8,6 +9,10 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
 };
 
